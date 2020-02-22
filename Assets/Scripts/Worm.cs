96b007a675c1036e2b790ec26843a6c6bc803926
Ray J. Greenwell <ray@bogocorp.com>
2020-02-21 17:16:35 -0800

    First pass at code to move the worm body.
    Works, but looks like shit. Floating point errors?

--- Assets/Scripts/Worm.cs
+++ Assets/Scripts/Worm.cs
@@ -13,8 +13,11 @@ public class Worm : MonoBehaviour
     // Skip index 0 because we are inside the "head", just set up the body segments
     for (int ii = 1; ii < length; ii++) {
       Vector3 offset = new Vector3(0, 0, -ii);
-      _segments.Add(Instantiate(segment, transform.position + offset, Quaternion.identity));
+      var seggy = Instantiate(segment, transform.position + offset, Quaternion.identity);
+      _segments.Add(new SegmentRecord(seggy));
     }
+
+    snapshotTarget();
   }
 
   void Update () {
@@ -30,14 +33,63 @@ public class Worm : MonoBehaviour
       }
     }
 
-    var tx = Vector3.forward * Time.deltaTime * speed;
+    var moveDistance = Time.deltaTime * speed;
+
+    // now, move the head forward
+    var tx = Vector3.forward * moveDistance;
     transform.Translate(tx);
-    foreach (GameObject segment in _segments) {
-      segment.transform.Translate(tx);
+
+    // if rotation changed, make a new Target.
+    if (turn != 0) snapshotTarget();
+
+    // now visit each segment and interpolate it towards its next target
+    foreach (var segment in _segments) {
+      var segMove = moveDistance;
+      var target = _targets[segment.targetIndex];
+      var pos = segment.segment.transform.localPosition;
+      do {
+        var distance = Vector3.Distance(target.position, pos);
+        segment.segment.transform.localPosition =
+            Vector3.MoveTowards(pos, target.position, segMove);
+        segMove -= distance;
+        if (segMove > 0) {
+          segment.targetIndex += 1;
+          // snapshot a new target if we haven't turned in a while...
+          if (segment.targetIndex == _targets.Count) snapshotTarget();
+          target = _targets[segment.targetIndex];
+          segment.segment.transform.rotation = Quaternion.Euler(0, target.rotation, 0);
+        }
+      } while (segMove > 0);
     }
+  }
+
+  /** Make a target with the head's current position and rotation. */
+  protected void snapshotTarget () {
+    _targets.Add(new Target(this.transform.localPosition, this._rotation));
   }
 
   protected float _rotation = 0;
 
-  protected IList<GameObject> _segments = new List<GameObject>();
+  private IList<SegmentRecord> _segments = new List<SegmentRecord>();
+  private IList<Target> _targets = new List<Target>();
+}
+
+class Target {
+  public Vector3 position;
+  public float rotation;
+
+  public Target (Vector3 pos, float rot) {
+    this.position = pos;
+    this.rotation = rot;
+  }
+}
+
+class SegmentRecord {
+  public GameObject segment;
+  public int targetIndex;
+
+  public SegmentRecord (GameObject segment) {
+    this.segment = segment;
+    this.targetIndex = 0;
+  }
 }
