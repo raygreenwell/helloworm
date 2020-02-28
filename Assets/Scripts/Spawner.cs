48352d575181de0b2b7814b406910908c5a6bee9
Ray J. Greenwell <ray@bogocorp.com>
2020-02-28 15:07:38 -0800

    Spawn new AI worms away from existing worms.
    The Spawner can be given a tag and radius to avoid gameobjects.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -14,6 +14,12 @@ public class Spawner : MonoBehaviour
 
   public Vector3 maxRotation = new Vector3(0, 0, 0);
 
+  public string avoidTag;
+
+  public float avoidRadius;
+
+  public int avoidTries = 10;
+
   [Range(0, Single.MaxValue)]
   public float frequency = 1;
 
@@ -22,9 +28,29 @@ public class Spawner : MonoBehaviour
     if (_accum < frequency) return;
 
     _accum -= frequency;
+    var loc = findGoodLocation();
+    if (loc == null) return;
     Instantiate(toSpawn,
-        rand(minPosition, maxPosition),
+        (Vector3)loc,
         Quaternion.Euler(rand(minRotation, maxRotation)));
+  }
+
+  protected Vector3? findGoodLocation () {
+    for (int tries = 0; tries < avoidTries; tries++) {
+      var loc = rand(minPosition, maxPosition);
+      var isGood = true;
+      if (avoidRadius > 0) {
+        var colliders = Physics.OverlapSphere(loc, avoidRadius);
+        foreach (var collider in colliders) {
+          if (collider.gameObject.tag == avoidTag) {
+            isGood = false;
+            break;
+          }
+        }
+      }
+      if (isGood) return loc;
+    }
+    return null;
   }
 
   protected Vector3 rand (Vector3 min, Vector3 max) {
