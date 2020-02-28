0b15bfbcc1a62ef6e702c45911464e9dd441d1a9
Ray J. Greenwell <ray@bogocorp.com>
2020-02-28 15:37:54 -0800

    Have AI worms make gentler turns too.
    I thought I had, but it turns out Random.Range() has an int overload
    that I was calling.

--- Assets/Scripts/AiWormSteering.cs
+++ Assets/Scripts/AiWormSteering.cs
@@ -4,7 +4,7 @@ public class AiWormSteering : MonoBehaviour
 {
   public void Update () {
     if (_turn == 0 && Random.Range(0, 50) == 0) {
-      _turn = Random.Range(-1, 1);
+      _turn = Random.Range(-1f, 1f);
       Invoke("resetTurning", Random.Range(.25f, 1.5f));
     }
     gameObject.GetComponent<Worm>().steer(_turn, _boost);
