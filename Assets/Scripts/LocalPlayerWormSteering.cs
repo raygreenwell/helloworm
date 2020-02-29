5157a62e0e9c037a869fd1c28a534bfe98d264f7
Ray J. Greenwell <ray@bogocorp.com>
2020-02-28 17:32:53 -0800

    Press left-ctrl to change cameras during gameplay.

--- Assets/Scripts/LocalPlayerWormSteering.cs
+++ Assets/Scripts/LocalPlayerWormSteering.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class LocalPlayerWormSteering : MonoBehaviour
 {
+  public Camera mainCamera;
+
+  public Camera overheadCamera;
+
   public void Start () {
     gameObject.GetComponent<Worm>().setPlayer(true);
   }
 
   public void Update () {
     gameObject.GetComponent<Worm>().steer(Input.GetAxis("Horizontal"), Input.GetButton("Jump"));
+
+    var fire = Input.GetButton("Fire1");
+    if (fire != _cameraChanged) {
+      if (fire) {
+        mainCamera.enabled = !mainCamera.enabled;
+        overheadCamera.enabled = !overheadCamera.enabled;
+      }
+      _cameraChanged = fire;
+    }
   }
+
+  protected bool _cameraChanged;
 }
